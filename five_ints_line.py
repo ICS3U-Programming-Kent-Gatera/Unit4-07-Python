88eebc20bacaa166330772ef74f78c8b39ccdc8d
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-11-20 21:55:09 +0000

    comment changes

--- five_ints_line.py
+++ five_ints_line.py
@@ -7,7 +7,7 @@
 def main():
     # I defined the integers in the range as int for readability.
     # +1 is due to how range is in python.
-    for int in range(1000, 2000+1, 1):
+    for int in range(1000, 2000 + 1, 1):
         # for every 5 numbers a new line will be created.
         if int % 5 == 0:
             print(int)
